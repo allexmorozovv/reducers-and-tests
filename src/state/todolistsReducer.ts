c29339d6dd1638d406b3da69f6142b06fb9cbc26
allexmorozovv <allexmorozovv@gmail.com>
2022-11-11 18:00:44 +0300

    add 'correct todolist should be added' test + reducer

--- src/state/todolistsReducer.ts
+++ src/state/todolistsReducer.ts
@@ -1,16 +1,22 @@
 import {FilterValuesType, TodolistType} from "../App";
+import {v1} from "uuid";
 
 export const todolistsReducer = (state: Array<TodolistType>, action: mainTodolistsType) => {
     switch (action.type) {
         case 'REMOVE-TODOLIST': {
             return state.filter(tl => tl.id !== action.payload.id)
         }
+        case "ADD-TODOLIST":{
+            let newTodolistId = v1();
+            let newTodolist: TodolistType = {id: newTodolistId, title:action.payload.title, filter: 'all'};
+            return [newTodolist,...state]
+        }
         default:
             return state
     }
 }
 
-type mainTodolistsType = removeTodolistACType
+type mainTodolistsType = removeTodolistACType | addTodolistACType
 
 type removeTodolistACType = ReturnType<typeof removeTodolistAC>
 export const removeTodolistAC = (todolistId1: string) => {
@@ -18,6 +24,14 @@ export const removeTodolistAC = (todolistId1: string) => {
         type: 'REMOVE-TODOLIST',
         payload: {id: todolistId1}
     } as const
+}
+
+type addTodolistACType=ReturnType<typeof addTodolistAC>
+export const addTodolistAC = (newTodolistTitle: string) => {
+  return{
+      type:'ADD-TODOLIST',
+      payload:{title: newTodolistTitle}
+  } as const
 }
 
 // type changeFilterACType = ReturnType<typeof changeFilterAC>
